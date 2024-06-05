d7d09ca90f4a07250d40fac9d934c6783aec4b15
Gyehyun Park <gyehyunbak@gmail.com>
2024-06-05 18:01:19 +0900

    first commit

---
+++ src/main/java/com/groovith/groovith/GroovithApplication.java
@@ -0,0 +1,13 @@
+package com.groovith.groovith;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GroovithApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GroovithApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/groovith/groovith/GroovithApplicationTests.java
@@ -0,0 +1,13 @@
+package com.groovith.groovith;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GroovithApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
