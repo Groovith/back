5d2c3fe6ff3244ec1db0509fd04ca7fafaa04e9a
Gyehyun Park <gyehyunbak@gmail.com>
2024-06-06 00:35:12 +0900

    feature: Security and JWT

---
+++ src/main/java/com/groovith/groovith/domain/auth/application/CustomUserDetailsService.java
@@ -0,0 +1,29 @@
+package com.groovith.groovith.domain.auth.application;
+
+import com.groovith.groovith.domain.auth.dto.CustomUserDetails;
+import com.groovith.groovith.domain.user.dao.UserRepository;
+import com.groovith.groovith.domain.user.domain.UserEntity;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+        UserEntity userData = userRepository.findByUsername(username);
+
+        if (userData != null) {
+            return new CustomUserDetails(userData);
+        }
+
+        return null;
+    }
+}
