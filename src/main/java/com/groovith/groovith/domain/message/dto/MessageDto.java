3b5937d6ff8011b6a7744b3243226da8ae8e1b8a
DESKTOP-1ROFC7Q\ksy13 <ksy13001@gmail.com>
2024-06-09 00:23:57 +0900

    통신에 사용할 메시지 dto

---
+++ src/main/java/com/groovith/groovith/domain/message/dto/MessageDto.java
@@ -0,0 +1,47 @@
+package com.groovith.groovith.domain.message.dto;
+
+import com.groovith.groovith.domain.chatRoom.domain.UserChatRoom;
+import com.groovith.groovith.domain.message.domain.Message;
+import com.groovith.groovith.domain.message.domain.MessageType;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import lombok.AccessLevel;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.transaction.annotation.Transactional;
+
+/**
+ * 통신에 사용할 메세지
+ * */
+@Data
+@Transactional
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class MessageDto {
+
+    private String content;
+
+    private Long chatRoomId;
+
+    private Long userId;
+
+    @Enumerated(EnumType.STRING)
+    private MessageType type;   //  CHAT, JOIN, LEAVE, PLAYER
+
+    public Message toEntity(UserChatRoom userChatRoom){
+        return Message.builder()
+                .content(content)
+                .messageType(type)
+                .userChatRoom(userChatRoom)
+                .build();
+    }
+//
+//    @Builder
+//    public Message(String content, UserChatRoom userChatRoom, MessageType messageType){
+//        this.content = content;
+//        this.userChatRoom = userChatRoom;
+//        this.messageType = messageType;
+//        userChatRoom.getMessages().add(this);
+//    }
+//
+
+}
