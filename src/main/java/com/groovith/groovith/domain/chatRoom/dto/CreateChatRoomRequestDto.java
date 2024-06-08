5e6d15c72a0935e87210e6b4e60174ab2d9317f3
DESKTOP-1ROFC7Q\ksy13 <ksy13001@gmail.com>
2024-06-09 00:25:42 +0900

    채팅방 생성 dto

---
+++ src/main/java/com/groovith/groovith/domain/chatRoom/dto/CreateChatRoomRequestDto.java
@@ -0,0 +1,64 @@
+package com.groovith.groovith.domain.chatRoom.dto;
+
+import com.groovith.groovith.domain.chatRoom.domain.ChatRoom;
+import com.groovith.groovith.domain.chatRoom.domain.ChatRoomStatus;
+import com.groovith.groovith.domain.chatRoom.domain.ChatRoomType;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ *  ChatRoom 생성 request dto
+ * */
+@Data
+@NoArgsConstructor
+public class CreateChatRoomRequestDto {
+    private Long userId;
+    private String name;
+    private ChatRoomStatus chatRoomStatus;  // "private" | "friends" | "public"
+    private ChatRoomType chatRoomType;     // "song" | "album" | "artist" | "playlist"
+
+    private Long songId;
+    private Long albumId;
+    private Long artistId;
+    private Long playlistId;
+
+    @Builder
+    public CreateChatRoomRequestDto(Long userId, String name, ChatRoomStatus chatRoomStatus, ChatRoomType chatRoomType,
+                                    Long songId, Long albumId, Long artistId, Long playlistId ){
+        this.userId = userId;
+        this.name = name;
+        this.chatRoomStatus = chatRoomStatus;   // PRIVATE, FRIENDS, PUBLIC
+        this.chatRoomType = chatRoomType;       //  SONG, ALBUM, ARTIST, PLAYLIST
+
+        this.songId = songId;
+        this.albumId = albumId;
+        this.artistId = artistId;
+        this.playlistId = playlistId;
+    }
+
+    public static class CreateChatRoomRequestDtoBuilder {
+        public CreateChatRoomRequestDtoBuilder typeId(ChatRoomType chatRoomType, Long typeId) {
+            this.chatRoomType = chatRoomType;
+            if (chatRoomType == ChatRoomType.SONG) {
+                this.songId(typeId);
+            } else if (chatRoomType == ChatRoomType.ALBUM) {
+                this.albumId(typeId);
+            } else if (chatRoomType == ChatRoomType.ARTIST) {
+                this.artistId(typeId);
+            } else if (chatRoomType == ChatRoomType.PLAYLIST) {
+                this.playlistId(typeId);
+            }
+            return this;
+        }
+    }
+
+    // ChatRoom 엔티티로 변환
+    public ChatRoom toEntity(){
+        ChatRoom chatRoom =  ChatRoom.builder()
+                            .name(name)
+                            .build();
+        return chatRoom;
+    }
+
+}
---
+++ src/main/java/com/groovith/groovith/domain/chatRoom/dto/CreateChatRoomResponseDto.java
@@ -0,0 +1,21 @@
+package com.groovith.groovith.domain.chatRoom.dto;
+
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ *  ChatRoom 생성시 response dto
+ * */
+
+@Data
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class CreateChatRoomResponseDto  {
+    private Long chatRoomId;
+
+    @Builder
+    public CreateChatRoomResponseDto(Long chatRoomId){
+        this.chatRoomId = chatRoomId;
+    }
+}
