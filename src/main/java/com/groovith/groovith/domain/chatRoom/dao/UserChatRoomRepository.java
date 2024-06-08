2ee62a88ee89f6b33d389f1d23ece48ab25700f1
DESKTOP-1ROFC7Q\ksy13 <ksy13001@gmail.com>
2024-06-09 03:35:30 +0900

    optional 로 변경

--- src/main/java/com/groovith/groovith/domain/chatRoom/dao/UserChatRoomRepository.java
+++ src/main/java/com/groovith/groovith/domain/chatRoom/dao/UserChatRoomRepository.java
@@ -3,6 +3,9 @@ package com.groovith.groovith.domain.chatRoom.dao;
 import com.groovith.groovith.domain.chatRoom.domain.UserChatRoom;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface UserChatRoomRepository extends JpaRepository<UserChatRoom, Long> {
-    UserChatRoom findByUserIdAndChatRoomId(Long userId, Long chatRoomId);
+    Optional<UserChatRoom> findByUserIdAndChatRoomId(Long userId, Long chatRoomId);
 }
