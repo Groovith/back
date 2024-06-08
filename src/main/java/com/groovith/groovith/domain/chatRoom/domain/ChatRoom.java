35f93b44ec271cb50c8172c13b9ea69a8c4af89c
DESKTOP-1ROFC7Q\ksy13 <ksy13001@gmail.com>
2024-06-09 03:31:34 +0900

    @GeneratedValue 수정

--- src/main/java/com/groovith/groovith/domain/chatRoom/domain/ChatRoom.java
+++ src/main/java/com/groovith/groovith/domain/chatRoom/domain/ChatRoom.java
@@ -20,7 +20,7 @@ import java.util.List;
 public class ChatRoom {
 
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "chatRoom_id")
     private Long id;
 
