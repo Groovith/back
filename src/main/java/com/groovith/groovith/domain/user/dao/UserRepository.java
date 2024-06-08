1b3a6184f46ed4e94c94c17011277a68bc16c395
DESKTOP-1ROFC7Q\ksy13 <ksy13001@gmail.com>
2024-06-09 00:22:29 +0900

    매서드추가

--- src/main/java/com/groovith/groovith/domain/user/dao/UserRepository.java
+++ src/main/java/com/groovith/groovith/domain/user/dao/UserRepository.java
@@ -2,10 +2,15 @@ package com.groovith.groovith.domain.user.dao;
 
 import com.groovith.groovith.domain.user.domain.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
-public interface UserRepository extends JpaRepository<UserEntity, Integer> {
+public interface UserRepository extends JpaRepository<UserEntity, Long> {
 
     Boolean existsByUsername(String username);
 
     UserEntity findByUsername(String username);
+
+    @Query("SELECT u FROM UserEntity u JOIN FETCH u.userChatRoom WHERE u.id = :userId")
+    UserEntity findByIdWithUserChatRoom(@Param("userId") Long userId);
 }
