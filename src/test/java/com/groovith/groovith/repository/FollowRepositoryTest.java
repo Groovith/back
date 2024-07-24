580616aea7caa4000e4456e846ed37ff6920b19f
DESKTOP-1ROFC7Q\ksy13 <ksy13001@gmail.com>
2024-07-24 14:46:08 +0900

    Follow 관련 클래스 단위 테스트 작성

---
+++ src/test/java/com/groovith/groovith/repository/FollowRepositoryTest.java
@@ -0,0 +1,88 @@
+package com.groovith.groovith.repository;
+
+import com.groovith.groovith.domain.Follow;
+import com.groovith.groovith.domain.StreamingType;
+import com.groovith.groovith.domain.User;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+public class FollowRepositoryTest {
+
+    @Autowired FollowRepository followRepository;
+    @Autowired UserRepository userRepository;
+
+    @Test
+    public void save(){
+        //given
+        User follower = createUser("follower", "1234");
+        User following = createUser("following", "1234");
+
+        Follow follow = createFollow(following, follower);
+        //when
+        Follow savedFollow = followRepository.save(follow);
+
+        //then
+        Assertions.assertThat(savedFollow).isEqualTo(follow);
+        Assertions.assertThat(savedFollow.getFollower()).isEqualTo(follow.getFollower());
+        Assertions.assertThat(savedFollow.getFollowing()).isEqualTo(follow.getFollowing());
+    }
+
+    @Test
+    public void existsByFollowerAndFollowing() throws Exception{
+        //given
+        User notFollower=  createUser("notFollower", "1234");
+        User follower = createUser("follower", "1234");
+        User following = createUser("following", "1234");
+        userRepository.save(follower);
+        userRepository.save(following);
+        userRepository.save(notFollower);
+
+        Follow follow = createFollow(following, follower);
+        followRepository.save(follow);
+        //when
+        Boolean exist = followRepository.existsByFollowerAndFollowing(follower, following);
+        Boolean notExist = followRepository.existsByFollowerAndFollowing(notFollower, following);
+        //then
+        Assertions.assertThat(exist).isEqualTo(true);
+        Assertions.assertThat(notExist).isEqualTo(false);
+    }
+
+    @Test
+    public void deleteByFollowerAndFollowing() throws Exception{
+        //given
+        User follower = createUser("follower", "1234");
+        User following = createUser("following", "1234");
+        userRepository.save(follower);
+        userRepository.save(following);
+
+        Follow follow = createFollow(following, follower);
+        followRepository.save(follow);
+
+        //when
+        followRepository.deleteByFollowerAndFollowing(follower, following);
+
+        //then
+        Assertions.assertThat(followRepository.existsByFollowerAndFollowing(follower, following)).isEqualTo(false);
+    }
+
+    public Follow createFollow(User following, User follower){
+        Follow follow = new Follow();
+        follow.setFollowing(following);
+        follow.setFollower(follower);
+        return follow;
+    }
+    public User createUser(String username, String password){
+        User data = new User();
+        data.setUsername(username);
+        data.setPassword(password);
+        data.setRole("ROLE_USER");
+        data.setStreaming(StreamingType.NONE);
+        return data;
+    }
+}
