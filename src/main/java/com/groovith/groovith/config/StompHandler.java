bce50c04cd53feee852608b347dc3f97a667ecb4
DESKTOP-1ROFC7Q\ksy13 <ksy13001@gmail.com>
2024-07-30 13:36:57 +0900

    stomp 인터셉터에서 토큰으로부터 userId 가져오기

--- src/main/java/com/groovith/groovith/config/StompHandler.java
+++ src/main/java/com/groovith/groovith/config/StompHandler.java
@@ -18,12 +18,17 @@ public class StompHandler implements ChannelInterceptor {
     private final JwtUtil jwtUtil;
 
 
+
     @Override
     public Message<?> preSend(Message<?> message, MessageChannel channel){
         StompHeaderAccessor accessor = StompHeaderAccessor.wrap(message);
         // 웹소켓 연결시 Stomp 메세지 헤더의 Authorization 에 담긴 jwtToken 검증
         if (StompCommand.CONNECT == accessor.getCommand()){
-            String userId = accessor.getFirstNativeHeader("userId");
+            // userId 를 토큰에서부터 받아오게끔 수정
+            Long userId = jwtUtil.getUserId(accessor.getFirstNativeHeader("access"));
+            //String userId = accessor.getFirstNativeHeader("userId");
+
+            log.info("userId:{}", userId);
             String token = accessor.getFirstNativeHeader("access");
 
             jwtUtil.validateToken(token, userId);
