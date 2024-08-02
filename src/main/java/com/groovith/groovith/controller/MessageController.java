c4a181ad3e8321620d8b333906e785138bbe5673
DESKTOP-1ROFC7Q\ksy13 <ksy13001@gmail.com>
2024-08-02 18:48:43 +0900

    메세지 createdAt 반환

--- src/main/java/com/groovith/groovith/controller/MessageController.java
+++ src/main/java/com/groovith/groovith/controller/MessageController.java
@@ -23,6 +23,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.format.DateTimeFormatter;
 import java.util.Objects;
 import java.util.Optional;
 
@@ -92,6 +93,7 @@ public class MessageController {
         messageResponseDto.setUsername(user.get().getUsername());
         messageResponseDto.setContent(message.getContent());
         messageResponseDto.setType(message.getMessageType());
+        messageResponseDto.setCreatedAt(message.getCreatedAt());
 
 
         template.convertAndSend("/sub/api/chat/" + messageDto.getChatRoomId(), messageResponseDto);
