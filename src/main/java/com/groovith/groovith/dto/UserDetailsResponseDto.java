87b12412a5e5059a4e5184acc4fe88014a2f59df
DESKTOP-1ROFC7Q\ksy13 <ksy13001@gmail.com>
2024-09-13 08:42:16 +0900

    팔로우 알림, 팔로우 수 표시 수정

--- src/main/java/com/groovith/groovith/dto/UserDetailsResponseDto.java
+++ src/main/java/com/groovith/groovith/dto/UserDetailsResponseDto.java
@@ -1,5 +1,6 @@
 package com.groovith.groovith.dto;
 
+import com.groovith.groovith.domain.Follow;
 import com.groovith.groovith.domain.FollowStatus;
 import com.groovith.groovith.domain.User;
 import lombok.AllArgsConstructor;
@@ -8,6 +9,7 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Getter
 @Setter
@@ -18,16 +20,17 @@ public class UserDetailsResponseDto {
     private String username;
     private FollowStatus status;    // NOFOLLOW | ACCEPTED | PENDING | REJECTED
     private String imageUrl;
-    private Integer following;
-    private Integer follower;
+    private Integer followingCount;
+    private Integer followerCount;
 
 
     public UserDetailsResponseDto(User user) {
         this.id = user.getId();
         this.username = user.getUsername();
-        this.status = FollowStatus.NOFOLLOW;   // 기본 상태는 팔로우 x
+        this.status = FollowStatus.NOFOLLOW;   // 기본 상태는 팔로우 x, 검색 결과에는 포함x
         this.imageUrl = user.getImageUrl();
-        this.follower = user.getFollowers().size();
-        this.following = user.getFollowing().size();
+        // 승인된 팔로우만 카운트
+        this.followingCount = user.getFollowing().stream().filter(f->f.getStatus()==FollowStatus.ACCEPTED).toList().size();
+        this.followerCount = user.getFollowers().stream().filter(f->f.getStatus()==FollowStatus.ACCEPTED).toList().size();
     }
 }
