42b9bda8dafd06b39599c188483f34a1a968aa10
DESKTOP-1ROFC7Q\ksy13 <ksy13001@gmail.com>
2024-09-22 23:56:13 +0900

    비밀번호 변경시 인증코드 만료시간 설정 및 자동 삭제 구현

--- src/main/java/com/groovith/groovith/domain/PasswordResetCertification.java
+++ src/main/java/com/groovith/groovith/domain/PasswordResetCertification.java
@@ -1,19 +1,19 @@
 package com.groovith.groovith.domain;
 
 import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.redis.core.RedisHash;
 
-@Entity
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
-@Table(name="password_reset_certification")
+@RedisHash(value = "passwordResetCertification", timeToLive = 300) // 유효기간 5분
 public class PasswordResetCertification {
     @Id
     private String email;
