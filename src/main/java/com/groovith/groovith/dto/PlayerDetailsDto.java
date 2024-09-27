06f36db30f69714142718c816a87b6fa887c0df4
DESKTOP-1ROFC7Q\ksy13 <ksy13001@gmail.com>
2024-09-27 20:46:05 +0900

    데이터 베이스 mySql 로 통합

--- src/main/java/com/groovith/groovith/dto/PlayerDetailsDto.java
+++ src/main/java/com/groovith/groovith/dto/PlayerDetailsDto.java
@@ -11,7 +11,7 @@ import java.util.List;
 @Data
 @NoArgsConstructor
 public class PlayerDetailsDto {
-    private String _id;
+    private Long id;
     private Long chatRoomId;
     private Integer userCount;
     private Integer currentPlaylistIndex;
