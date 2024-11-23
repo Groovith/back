e3145b61eaaa5cf3c88c7d4cf54f035f32e436cf
Gyehyun Park <gyehyunbak@gmail.com>
2024-11-23 15:31:03 +0900

    fix: UpdateChatRoomRequestDto에 오타 수정

--- src/main/java/com/groovith/groovith/dto/UpdateChatRoomRequestDto.java
+++ src/main/java/com/groovith/groovith/dto/UpdateChatRoomRequestDto.java
@@ -7,6 +7,6 @@ import lombok.Getter;
 @Getter
 public class UpdateChatRoomRequestDto {
     private String name;
-    private ChatRoomPrivacy status;
+    private ChatRoomPrivacy privacy;
     private ChatRoomPermission permission;
 }
